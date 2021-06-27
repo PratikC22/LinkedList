535683b79a368164529766f5e410a18b6305bfde
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-27 09:39:10 +0530

    Appended 70 and 30 to 56

--- Runner.java
+++ Runner.java
@@ -1,11 +1,17 @@
+/************************************************************************
+ *  In this code for linked list int 70 and 36 are appended to 56.
+ *  
+ *  @author Pratik Chaudhari
+ *  @since 25/06/2021
+ ************************************************************************/
 package com.bridgelabz;
 
 public class Runner {
 	public static void main(String[] args) {
 		LinkedList<Integer> numberList = new LinkedList<Integer>();
-		numberList.addAtStart(70);
-		numberList.addAtStart(30);
-		numberList.addAtStart(56);
+		numberList.add(56);
+		numberList.add(30);
+		numberList.add(70);
 		numberList.show();
 	}
 }
