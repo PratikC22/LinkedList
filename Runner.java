c3c28110c043e81ccfa437a12fdf9fdbfdceb9c6
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-27 09:29:23 +0530

    Created a simple linked list of 56,30,70

---
+++ Runner.java
@@ -0,0 +1,18 @@
+/************************************************************************
+ *  In this code a simple linked list of integers is created, with 3 
+ *  int 56,30 and 70.
+ *  
+ *  @author Pratik Chaudhari
+ *  @since 25/06/2021
+ ************************************************************************/
+package com.bridgelabz;
+
+public class Runner {
+	public static void main(String[] args) {
+		LinkedList<Integer> numberList = new LinkedList<Integer>();
+		numberList.add(56);
+		numberList.add(30);
+		numberList.add(70);
+		numberList.show();
+	}
+}
