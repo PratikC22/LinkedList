43bb1ef97168c15ebd338f8a3c25ef2b34730aa9
Pratik Chaudhari <pratikc1020@gmail.com>
2021-06-27 10:00:37 +0530

    use case 9 to delete node is executed

--- Runner.java
+++ Runner.java
@@ -1,9 +1,10 @@
-/****************************************************************************
- *  In this code a simple linked list of integers, search method is executed.
+/************************************************************************
+ *  In this code a simple linked list of integers is created, with 3 
+ *  int 56,30,40,70 and 40 is deleted.
  *  
  *  @author Pratik Chaudhari
  *  @since 25/06/2021
- ****************************************************************************/
+ ************************************************************************/
 package com.bridgelabz;
 
 public class Runner {
@@ -11,12 +12,14 @@ public class Runner {
 		LinkedList<Integer> numberList = new LinkedList<Integer>();
 		numberList.add(56);
 		numberList.add(30);
+		numberList.add(40);
 		numberList.add(70);
 		System.out.println("Linked list elements");
 		numberList.show();
 		System.out.println();
-		int searchResult = numberList.searchByValue(90);
-		String result = (searchResult == -1) ? "value not found in the list" : "value found at index " + searchResult;
-		System.out.println(result);
+		int searchResult = numberList.searchByValue(40);
+		numberList.popAtIndex(searchResult);
+		numberList.show();
+		System.out.println("The size of the list is " + numberList.size());
 	}
 }
